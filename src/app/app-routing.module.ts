4b76bb1114a38b08f1bc63a8f0f8a10634ff995a
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2024-02-13 05:08:38 +0200

    protect routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { MovieComponent } from './components/movie/movie.component';
+import { CarouselCardsComponent } from './components/carousel-cards/carousel-cards.component';
+import { AuthComponent } from './pages/authenticate/auth.component';
+import { AccountComponent } from './pages/account/account.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AuthGuard } from './guards/auth.guard';
+
+const routes: Routes = [
+  { path: '', canActivate: [AuthGuard], component: HomeComponent },
+  { path: 'auth', canActivate: [AuthGuard], component: AuthComponent },
+  { path: 'account', canActivate: [AuthGuard], component: AccountComponent },
+  { path: 'test', component: MovieComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router,
+} from '@angular/router';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean {
+    if (state.url === '/auth') {
+      if (localStorage.getItem('token')) {
+        this.router.navigate(['/']);
+        return false;
+      } else {
+        return true;
+      }
+    }
+    if (!localStorage.getItem('token')) {
+      this.router.navigate(['/auth']);
+      return false;
+    }
+    return true;
+  }
+}
